f4e73f42e78090bc31cfa76539272269f674ed02
sandro.coelho <sandro.coelho@afferolab.com.br>
2016-11-05 19:41:34 -0200

    First commit

---
+++ src/main/java/org/nlp2rdf/JSONLDContext.java
@@ -0,0 +1,13 @@
+package org.nlp2rdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+
+public class JSONLDContext {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JSONLDContext.class, args);
+    }
+
+}
---
+++ src/main/java/org/nlp2rdf/rest/Context.java
@@ -0,0 +1,59 @@
+package org.nlp2rdf.rest;
+
+import com.google.common.base.Preconditions;
+import org.apache.commons.io.IOUtils;
+import org.nlp2rdf.ContextJSONLD;
+import org.nlp2rdf.NIFFormat;
+import org.nlp2rdf.nif21.impl.NIF21;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
+
+@RestController
+public class Context {
+
+    @ResponseBody
+    @CrossOrigin
+    @RequestMapping(value = "/context", method = RequestMethod.GET)
+    public String getContext(@RequestParam(name = "ontology") String ontology,
+                             @RequestParam(name = "template", required = false) String template) {
+
+        Preconditions.checkNotNull(ontology);
+
+        ContextJSONLD context = new NIF21();
+        Set<String> ontologies = new HashSet<>();
+
+        ontologies.add(ontology);
+
+        String templateFileName = getTemplate(template);
+
+        if (!templateFileName.isEmpty()) {
+            return context.getContextForJSONLD(ontologies, templateFileName);
+        }
+
+        return context.getContextForJSONLD(ontologies);
+    }
+
+
+    private String getTemplate(String template) {
+        if (template != null && !template.isEmpty()) {
+            try {
+                String result = IOUtils.toString(new URL(template));
+                String fileName = template.replace("http://", "").replace("/", "").replace(":", "").replace("-","").replace(".","");
+                fileName = String.format(NIFFormat.TEMPLATE_ROOT.concat("%s"), fileName);
+                Files.write(Paths.get(fileName), result.getBytes());
+                return fileName;
+            } catch (Exception e) {
+                ;
+            }
+        }
+        return "";
+
+    }
+
+
+}
